8199ecf165c26f814fa5ec66b64eb561784ea083
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2021-08-27 18:26:24 +0530

    Add images and video data

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video-data`,
+        path: `${__dirname}/src/video-data`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-styled-components`,
-
+    `gatsby-transformer-remark`,
+    `gatsby-background-image-es5`,
+    {
+      resolve: 'gatsby-background-image-es5',
+      options: {
+        specialChars: '/:',
+      },
+    },
+  ]
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
         <h1>
           <LogoLink to="/">Gatsby VideoPlayer</LogoLink>
         </h1>
-        </HeaderWrapper>
+        </HeaderWrapper>  
     </HeaderContainer>
   )
 }
